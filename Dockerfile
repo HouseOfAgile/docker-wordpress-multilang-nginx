acca10088b788ea7833c84a3da17f7e4ae856f42
jmeyo <jc.meillaud@gmail.com>
2014-06-05 19:38:46 +0200

    Integrate Phusion base-image, rework init, add ssh private key handling, add multilanguage support to wordpress

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,19 @@
-FROM ubuntu:14.04
-MAINTAINER Eugene Ware <eugene@noblesamurai.com>
+FROM phusion/baseimage:0.9.10
 
-# Keep upstart from complaining
-RUN dpkg-divert --local --rename --add /sbin/initctl
-RUN ln -sf /bin/true /sbin/initctl
+MAINTAINER Eugene Ware <eugene@noblesamurai.com>
+MAINTAINER Jean-Christophe Meillaud <jc@houseofagile.com>
+
+ENV HOME /root
+
+## Install SSH for a specific user (thanks to public key)
+ADD ./config/id_rsa.pub /tmp/your_key
+RUN cat /tmp/your_key >> /root/.ssh/authorized_keys && rm -f /tmp/your_key
+
+ENV APP_NAME houseofagile
+ENV MYSQL_HOST 172.17.0.3
+ENV MYSQL_USER admin
+ENV MYSQL_PASSWORD 6rSmSUKLqvMe
+ENV WORDPRESS_LANG fr_FR
 
 # Let the conatiner know that there is no tty
 ENV DEBIAN_FRONTEND noninteractive
@@ -12,10 +22,10 @@ RUN apt-get update
 RUN apt-get -y upgrade
 
 # Basic Requirements
-RUN apt-get -y install mysql-server mysql-client nginx php5-fpm php5-mysql php-apc pwgen python-setuptools curl git unzip
+RUN apt-get -y install nginx php5-fpm php5-mysql php-apc pwgen python-setuptools curl git unzip
 
-# Wordpress Requirements
-RUN apt-get -y install php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+# Wordpress Requirements and mysql client
+RUN apt-get -y install mysql-client php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 
 # mysql config
 RUN sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
@@ -37,8 +47,10 @@ RUN find /etc/php5/cli/conf.d/ -name "*.ini" -exec sed -i -re 's/^(\s*)#(.*)/\1;
 ADD ./nginx-site.conf /etc/nginx/sites-available/default
 
 # Supervisor Config
+RUN apt-get install -y supervisor
 RUN /usr/bin/easy_install supervisor
 RUN /usr/bin/easy_install supervisor-stdout
+RUN mkdir -p /var/log/supervisor
 ADD ./supervisord.conf /etc/supervisord.conf
 
 # Install Wordpress
@@ -47,14 +59,17 @@ RUN cd /usr/share/nginx/ && tar xvf latest.tar.gz && rm latest.tar.gz
 RUN mv /usr/share/nginx/html/5* /usr/share/nginx/wordpress
 RUN rm -rf /usr/share/nginx/www
 RUN mv /usr/share/nginx/wordpress /usr/share/nginx/www
+
+# Add localization here
+ADD wordpress-languages/ /usr/share/nginx/www/wp-content/languages/
 RUN chown -R www-data:www-data /usr/share/nginx/www
 
 # Wordpress Initialization and Startup Script
-ADD ./start.sh /start.sh
-RUN chmod 755 /start.sh
-
+RUN mkdir -p /etc/my_init.d
+ADD init_wordpress.sh /etc/my_init.d/init_wordpress.sh
+ADD supervisor.sh /etc/my_init.d/supervisor.sh
 # private expose
-EXPOSE 3306
 EXPOSE 80
 
-CMD ["/bin/bash", "/start.sh"]
+CMD ["/sbin/my_init"]
+
--- start.sh
+++ start.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
-  #mysql has to be started this way as it doesn't work to call from /etc/init.d
-  /usr/bin/mysqld_safe & 
-  sleep 10s
   # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
-  WORDPRESS_DB="wordpress"
-  MYSQL_PASSWORD=`pwgen -c -n -1 12`
-  WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
-  #This is so the passwords show up in logs. 
-  echo mysql root password: $MYSQL_PASSWORD
-  echo wordpress password: $WORDPRESS_PASSWORD
-  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-  echo $WORDPRESS_PASSWORD > /wordpress-db-pw.txt
+  WORDPRESS_DB_NAME="wordpress_$APP_NAME"
+  WORDPRESS_DB_USER="wp_$APP_NAME"
+  WORDPRESS_DB_PASSWORD=`pwgen -c -n -1 12`
 
-  sed -e "s/database_name_here/$WORDPRESS_DB/
-  s/username_here/$WORDPRESS_DB/
-  s/password_here/$WORDPRESS_PASSWORD/
+  sed -e "s/database_name_here/$WORDPRESS_DB_NAME/
+  s/username_here/$WORDPRESS_DB_USER/
+  s/password_here/$WORDPRESS_DB_PASSWORD/
+  s/localhost/$MYSQL_HOST/
+  s/define('WPLANG', '');/define('WPLANG', '$WORDPRESS_LANG');/
   /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
@@ -24,6 +18,7 @@ if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
   /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
+
 
   # Download nginx helper plugin
   curl -O `curl -i -s http://wordpress.org/plugins/nginx-helper/ | egrep -o "http://downloads.wordpress.org/plugin/[^']+"`
@@ -47,11 +42,15 @@ ENDL
 
   chown www-data:www-data /usr/share/nginx/www/wp-config.php
 
-  mysqladmin -u root password $MYSQL_PASSWORD
-  mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
-  killall mysqld
+  echo "Create Database"
+  mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e "DROP DATABASE IF EXISTS $WORDPRESS_DB_NAME;CREATE DATABASE $WORDPRESS_DB_NAME;"
+  echo "Add user $WORDPRESS_DB_USER"
+  mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD'; FLUSH PRIVILEGES;"
 fi
+#This is so the passwords show up in logs. 
+echo "Wordpress installed for $APP_NAME"
+echo "- Wordpress User created: $WORDPRESS_DB_USER"
+echo "- Mysql Database created: $WORDPRESS_DB_NAME"
+echo "- Wordpress User password: $WORDPRESS_DB_PASSWORD"
 
-# start all the services
-/usr/local/bin/supervisord -n
+#/usr/local/bin/supervisord -n
